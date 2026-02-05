1000613790016beb34a637676eaff5eab867b9ce
Rubenalvaareezz <rubalvrub@alumn.us.es>
2026-02-05 20:32:54 +0100

    Mi primer commit en eclipse, Java

---
+++ src/prueba1/prueba2.java
@@ -0,0 +1,10 @@
+package prueba1;
+
+public class prueba2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HolaMundo");
+		System.out.println("Me llamo Ruben");	
+	}
+}
